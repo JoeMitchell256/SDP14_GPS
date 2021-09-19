2eac516fbe137be3cd8abd30b55e66338dfa3a2f
Joseph Mitchell <70342721+JoeTheBro256@users.noreply.github.com>
2021-09-19 17:44:35 -0400

    Trying a new method to call function

--- function2.js
+++ function2.js
@@ -1,13 +1,22 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 var n, pulsefunction;
+var sum;
 
 function getInputValue(){
   n = document.getElementById("n").value;
   pulsefunction = document.getElementById("n").value;
 }
 
+
+$.getscript("function1.js",function(){
+  sum = func1(  parseInt(n) + parseInt(pulsefunction) );
+  ctx.fillText("Sum: " + sum, 100, 100 ); //Should display the sum as a result of input from n text box and pulsefunction textbox
+});
+
+/*
 function func2(){
-  var sum = func1( parseInt(n) + parseInt(pulsefunction) );
+  var sum = .func1( parseInt(n) + parseInt(pulsefunction) );
   ctx.fillText("Sum: " + sum, 100, 100 ); //Should display the sum as a result of input from n text box and pulsefunction textbox
 }
+*/
