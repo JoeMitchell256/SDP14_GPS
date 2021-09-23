1900399f0ec8b454b1337307db1794607807c22d
Joseph Mitchell <70342721+JoeTheBro256@users.noreply.github.com>
2021-09-22 20:27:16 -0400

    Add a findXpos function
    This function will locate the proper location for the y-axis on the graph.

--- content.js
+++ content.js
@@ -11,6 +11,31 @@ var xstart = 100;
 var xend = 1300;
 //The above variables are for testing purposes for the moment as I draw the x-axis
 
+/**
+Function findXpos - find the proper position for the y-axis given the x limits
+*/
+
+function findXpos(min,max){
+  var xpos = xstart;
+  var min = parseFloat(xMin);
+  var max = parseFloat(xMax);
+  
+  if(min > 0 && max > 0){
+    xpos = xstart;
+  }else if(min < 0 && max < 0){
+    xpos = xend;
+  }else{
+    xpos = xstart;
+  }
+  return xpos;
+}
+
+
+
+/**
+Function - drawXaxis will draw all content related to the x axis creation for the graph
+*/
+
 function drawXaxis(){
   //Setup the canvas drawing parameters (These are extremely important!)
   ctx.beginPath();
@@ -19,7 +44,12 @@ function drawXaxis(){
   ctx.font = "30px serif";
   
   ctx.fillText("n: " + n, 1000, 20); //Display value of n at the top right corner of page
-  ctx.stroke();
+  
+  var xpos = this.findXpos(xMin,xMax);
+  
+  ctx.fillText("xpos: " + xpos, 30, 20);
+  
+  ctx.stroke();//This will draw all content on the context to the canvas
 }
 
 
